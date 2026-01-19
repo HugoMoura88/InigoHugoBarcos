0baf7693eddf6be251a0ae1e22c18f59abde0b0e
1DAM <1DAM@A11-PC15>
2026-01-19 12:27:02 +0100

    asd

--- InigoHugoBarcos/src/Main/Main.java
+++ InigoHugoBarcos/src/Main/Main.java
@@ -13,9 +13,13 @@ public class Main {
 		
 		Motor motor1 = new Motor("cyan", 20, 40);
 		Motor motor2 = new Motor("rojo", 30, 20);
-		Motor motor3 = new Motor("verde", 10, 60);ç
+		Motor motor3 = new Motor("verde", 10, 60);
 		
-		Yate yate1 = new Yate("Yatote", motor3, 0);
+		Yate yate1 = new Yate("Yatote", motor3, 2.3,3);
+		Yate yate2 = new Yate("Yatin", motor2, 4.5,2);
+		
+		Catamaran catamaran1 = new Catamaran("Cata", motor2, 5.7, motor3);
+		Catamaran catamaran2 = new Catamaran("Cata", motor1, 7.5, motor2);
 		
 		
 		
