434664c4fecaf3de95def913e27cc8c01b6c4983
1DAM <1DAM@A11-PC15>
2026-01-19 12:09:21 +0100

    metodo

---
+++ InigoHugoBarcos/src/Main/Main.java
@@ -0,0 +1,25 @@
+package Main;
+
+
+import Clases.Barco;
+import Clases.Puerto;
+import Clases.Catamaran;
+import Clases.Yate;
+
+import java.util.ArrayList;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+		ArrayList<Barco> lista = new ArrayList<Barco>();
+		
+		
+		
+		Barco barco1 = new Barco();
+		
+}
