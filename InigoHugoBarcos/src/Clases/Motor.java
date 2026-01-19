8f274a2afec43db60f90ae7c3b06138687f855b1
HugoMoura88 <1DAM@A11-PC16>
2026-01-19 11:30:23 +0100

    Motor1

---
+++ InigoHugoBarcos/src/Clases/Motor.java
@@ -0,0 +1,43 @@
+package Clases;
+
+public class Motor {
+	//Atributos
+	protected String modelo;
+	protected int potencia;
+	protected double consumo;
+	//Constructores
+	public Motor(String modelo, int potencia, double consumo) {
+		super();
+		this.modelo = modelo;
+		this.potencia = potencia;
+		this.consumo = consumo;
+	}
+	//GetterSetters
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public int getPotencia() {
+		return potencia;
+	}
+	public void setPotencia(int potencia) {
+		this.potencia = potencia;
+	}
+	public double getConsumo() {
+		return consumo;
+	}
+	public void setConsumo(double consumo) {
+		this.consumo = consumo;
+	}
+	//ToString
+	@Override
+	public String toString() {
+		return "Motor [modelo=" + modelo + ", potencia=" + potencia + ", consumo=" + consumo + "]";
+	}
+	
+	
+	
+	
+}
